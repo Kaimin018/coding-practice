39fd77b4e632df76f592dde9d3097b53c555ff0f
Kaimin018 <58111621+Kaimin018@users.noreply.github.com>
2021-04-14 16:22:00 +0800

    Create uva_11332.cpp

---
+++ uva_11332.cpp
@@ -0,0 +1,24 @@
+// uva-11332
+#include <bits/stdc++.h>
+using namespace std;
+
+int64_t sum_digit(int64_t a){
+    
+    int64_t sum=0;
+    for (int i=a; i>0; i/=10){
+        sum += i%10;
+    }
+    if (sum > 10) return sum_digit(sum);
+    else return sum;
+}
+
+int main()
+{
+    int64_t a;
+    cin >> a;
+    while (a != 0){
+        cout << sum_digit(a) << endl;
+        cin >> a;
+    }
+    return 0;
+}
